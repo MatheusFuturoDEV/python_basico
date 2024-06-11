57bce924197ea5425fe92da2d8bf5d60306ccc6b
MatheusFuturoDEV <121911044+MatheusFuturoDEV@users.noreply.github.com>
2024-06-10 22:01:24 -0300

    Add files via upload

---
+++ contador de caracteres.py
@@ -0,0 +1,7 @@
+while True:
+    palavra = input('Digite uma palavra: ')
+    palavra_2 = palavra
+    resultado = len(palavra)
+    print(f'a palavra "{palavra_2}" tem {resultado} letras. ')
+
+    #contador de caracteres simples
---
+++ verificador de idade.py
@@ -0,0 +1,10 @@
+while True:
+    
+    idade = input('Digite sua idade: ')
+    idade = int(idade) #convertendo o valor da variavel para inteiro
+    if idade >=18:
+        print('Você é maior de idade')
+    elif idade <=18:
+        print('Você é menor de idade')
+
+#programa simples para verificar maioriade
